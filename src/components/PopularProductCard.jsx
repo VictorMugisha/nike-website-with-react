b931d3f19d4a93e70dec1310fc1af5fc67b968cd
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-05-30 16:24:51 +0200

    Progressed a bit

---
+++ src/components/PopularProductCard.jsx
@@ -0,0 +1,10 @@
+
+const PopularProductCard = () => {
+  return (
+    <div className="flex flex-col flex-1 w-full max-sm:w-full">
+        <img />
+    </div>
+  )
+}
+
+export default PopularProductCard
